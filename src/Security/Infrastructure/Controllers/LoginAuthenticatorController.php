7be8f92e8a9a27e69a49c33241b461b7c38e6274
jprous85 <jprous85@gmail.com>
2024-08-04 03:35:28 +0200

    create login method

--- src/Security/Infrastructure/Controllers/LoginAuthenticatorController.php
+++ src/Security/Infrastructure/Controllers/LoginAuthenticatorController.php
@@ -2,20 +2,40 @@
 
 namespace App\Security\Infrastructure\Controllers;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Shared\Infrastructure\Controller\BaseController;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 
-final class LoginAuthenticatorController extends AbstractController
+final class LoginAuthenticatorController extends BaseController
 {
-    public function __construct(
-    )
-    {
-    }
 
     public function login(
+        Request                     $request,
+        JWTTokenManagerInterface    $jwtManager,
+        UserPasswordHasherInterface $passwordHashed,
+        EntityManagerInterface      $em
     ): JsonResponse
     {
-        return new JsonResponse(['token' => 'token']);
+        $requestData = json_decode($request->getContent(), true, JSON_THROW_ON_ERROR);
+        $user = $em->getRepository(User::class)->findOneBy(['email' => $requestData['email']]);
+
+        if (!$user) {
+            throw new BadCredentialsException('User not exist');
+        }
+
+        if (!$passwordHashed->isPasswordValid($user, $requestData['password'])) {
+            throw new AccessDeniedHttpException('Bad credentials');
+        }
+
+        $token = $jwtManager->create($user);
+
+        return $this->successResponse(['token' => $token]);
     }
 }
